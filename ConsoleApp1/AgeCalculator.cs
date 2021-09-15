db0ec64f1093c97a4194a2e56602cd974b2b8228
mostmand <mostmand.mh@gmail.com>
2021-09-15 17:11:55 +0430

    first commit

---
+++ ConsoleApp1/AgeCalculator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class AgeCalculator
+    {
+        public int CalculateAge(DateTime dateOfBirth)
+        {
+            var now = DateTime.Now;
+            var difference = now - dateOfBirth;
+            return difference.Days / 365;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
