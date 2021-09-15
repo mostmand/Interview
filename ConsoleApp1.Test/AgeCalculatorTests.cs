068d6f2c10caf96825ae5298966b191500f3de8d
mostmand <mostmand.mh@gmail.com>
2021-09-15 17:43:48 +0430

    Add test

---
+++ ConsoleApp1.Test/AgeCalculatorTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Xunit;
+
+namespace ConsoleApp1.Test
+{
+    public class AgeCalculatorTests
+    {
+        [Fact]
+        public void CalculateAge_WhenDateOfBirthIsGiven_AgeBeCalculated()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
